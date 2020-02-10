87930bd89bc4b787e250800257a19d00c468fbc0
stevenks17 <stevenks17@gmail.com>
2020-02-10 13:47:09 -0500

    3rd commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,58 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "oreos"
   end
-
+  
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/signup'
+    end
+  end
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/signup"
+    else
+      redirect to "/failure"
+    end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
+
+
+
--- config.ru
+++ config.ru
@@ -1,4 +1,5 @@
 require './config/environment'
+require './app/controllers/application_controller'
 
 if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
