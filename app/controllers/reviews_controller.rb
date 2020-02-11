dcbf9a7a6cefb8daa1d5dd823bea849da1135dc8
stevenks17 <stevenks17@gmail.com>
2020-02-11 15:53:47 -0500

    4th commit(review_controller)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,51 @@
 class ReviewsController < ApplicationController
 
+    get '/review_entries' do 
+        @review_entries = Review.all
+        erb :'review_entries/index'
+    end
 
+    get 'review_entries/new' do
+        redirect_if_not_logged_in
+        erb :'review_entries/new'
+    end
+
+    post 'review_entries' do
+        redirect_if_not_logged_in
+        if params[:content] != ""
+            @review_entry = Review.create(content: params[:content], user_id: current_user.id)
+            redirect "review_entries/#{@review_entry.id}"
+        else
+            redirect 'review_entries/new'
+        end
+    end
+
+    get 'review_entries/:id/edit' do
+        redirect_if_not_logged_in
+        set_review_entry
+        if @review_entry == current_user && params[:content] != ""
+            @review_entry.update(content: params[:content])
+            redirect "/review_entries/#{@review_entry.id}"
+        else
+            redirect "users/#{current_user.id}"
+        end
+    end
+
+    delete '/review_entries/:id' do
+        set_review_entry
+        if authorized_to_edit?(@review_entry)
+            @review_entry.destroy
+            redirect '/review_entries'
+        else
+            redirect '/review_entries'
+        end
+    end
+
+    private
+
+    def set_review_entry
+        @review_entry = Review.find(params[:id])
+    end
 
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,43 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
    
-    get "/signup" do
-        erb :signup
-      end
+  get "/signup" do
+    erb :signup
+  end
     
-      post "/signup" do
-        if params[:username] == "" || params[:password] == ""
-          redirect '/failure'
-        else
-          User.create(username: params[:username], password: params[:password])
-          redirect '/signup'
-        end
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+     User.create(username: params[:username], password: params[:password])
+        redirect '/signup'
       end
+    end
     
-      get "/login" do
-        erb :login
-      end
+  get "/login" do
+    erb :login
+  end
     
-      post "/login" do
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect to "/signup"
-        else
-          redirect to "/failure"
-        end
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+        redirect to "/signup"
+      else
+        redirect to "/failure"
       end
+    end
     
-      get "/failure" do
-        erb :failure
-      end
+  get "/failure" do
+    erb :failure
+  end
     
-      get "/logout" do
-        session.clear
-        redirect "/"
-      end
+  get "/logout" do
+    session.clear
+     redirect "/"
+  end
     
 
-      end
+      
 
 
 
