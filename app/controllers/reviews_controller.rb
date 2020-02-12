8e3f597459d371d4f7aa0465a684d4f0951fbbaf
stevenks17 <stevenks17@gmail.com>
2020-02-12 16:34:04 -0500

    11th commit(Layout link added)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,8 +15,8 @@ class ReviewsController < ApplicationController
     post '/review_entries' do
         redirect_if_not_logged_in
         if params[:content] != ""
-            @review_entry = Review.create(content: params[:content], user_id: current_user.id, title: params[:title])
-            redirect "review_entries/#{@review_entry.id}"
+            @review_info = Review.create(content: params[:content], user_id: current_user.id, title: params[:title])
+            redirect "review_entries/#{@review_info.id}"
         else
             redirect 'review_entries/new'
         end
@@ -25,9 +25,9 @@ class ReviewsController < ApplicationController
     get '/review_entries/:id/edit' do
         redirect_if_not_logged_in
         set_review_entry
-        if @review_entry == current_user && params[:content] != ""
-            @review_entry.update(content: params[:content])
-            redirect "/review_entries/#{@review_entry.id}"
+        if @review_info == current_user && params[:content] != ""
+            @review_info.update(content: params[:content])
+            redirect "/review_entries/#{@review_info.id}"
         else
             redirect "users/#{current_user.id}"
         end
@@ -35,8 +35,8 @@ class ReviewsController < ApplicationController
 
     delete '/review_entries/:id' do
         set_review_entry
-        if authorized_to_edit?(@review_entry)
-            @review_entry.destroy
+        if authorized_to_edit?(@review_info)
+            @review_info.destroy
             redirect '/review_entries'
         else
             redirect '/review_entries'
@@ -46,7 +46,7 @@ class ReviewsController < ApplicationController
     private
 
     def set_review_entry
-        @review_entry = Review.find(params[:id])
+        @review_info = Review.find(params[:id])
     end
 
     
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,27 @@
 class UsersController < ApplicationController
    
-  get "/signup" do
+  get "/login" do
+    redirect_if_logged_in
+    erb :'/login'
+  end
+   
+  post '/login' do
+
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect "users/#{@user.id}"
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/signup' do
+    redirect_if_logged_in
     erb :signup
   end
     
-  post "/signup" do
-    if params[:username] == "" || params[:password] == ""
-      redirect '/failure'
-    else
-     User.create(username: params[:username], password: params[:password])
-        redirect '/signup'
-      end
-    end
-    
-  get "/login" do
-    erb :login
-  end
-    
-  post "/login" do
-    @user = User.find_by(username: params[:username])
-      if @user && @user.authenticate(params[:password])
-       session[:user_id] = @user.id
-        redirect to "/signup"
-      else
-        redirect to "/failure"
-      end
-    end
+
 
   post 'users' do
     @user = User.new(params)
